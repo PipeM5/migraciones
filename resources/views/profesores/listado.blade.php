c3ae71f413fcea99aceda7e7845c1f8193c8fd4e
jairoc11 <91041728+jairoc11@users.noreply.github.com>
2023-06-23 23:32:44 -0500

    Commit a la vista al listado de profesores.php

--- resources/views/profesores/listado.blade.php
+++ resources/views/profesores/listado.blade.php
@@ -4,21 +4,34 @@
 
 @section('content_header')
     <h1>Profesores</h1>
-
+ 
 
 @stop
 
 @section('content')
     <p>Listado de Profesores</p>
+    <a  class="btn btn-primary" href="/profesores/registrar">Adicionar</a>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Codigo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Catalogo</th>
+      <th scope="col">Opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    @php $contador =0 @endphp
+    @foreach ($profesores as $f)
+    @php $contador +=1 @endphp
 
 
+    
 
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+    <tr>
+      <th scope="row">{{$contador}}</th>
+      <td>{{$f->cod_profesor}}</td>
+      <td>{{$f->nom_profesor}}</td>
+      <td>{{$f->cat_profesor}}</td>
+      <td>@mdo…
